ca9ef8414bf9a5ff258805fad44483f3c766d479
José Antonio González Morente <jgonzalezmorente@gmail.com>
2020-11-18 11:27:05 +0100

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './virtual/virtual.component';
+import { DragComponent } from './drag/drag.component';
+import { PaisesComponent } from './paises/paises.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent,
+    DragComponent,
+    PaisesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollingModule,
+    DragDropModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
